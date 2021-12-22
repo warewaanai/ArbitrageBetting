e87a6a0f61ee558039a46c01a6ee83ae6a34baf2
Serban Cercelescu <warewaanai@gmail.com>
2021-12-22 21:42:48 +0200

    deploy attempt

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from typing import List
 from flask import Flask, jsonify, send_from_directory
 from flask_cors import CORS
 from dotenv import load_dotenv
-from setup import setup
+import sqlite3
 import os
 
 from structures.bundle import Bundle
@@ -49,7 +49,49 @@ def serve(path):
     return send_from_directory(str(app.static_folder), 'index.html')
 
 
+def setup():
+    create_bundles = """
+    CREATE TABLE ODDS_BUNDLE
+         (
+         NAME           TEXT      NOT NULL,
+         DESCRIPTION    TEXT      NOT NULL,
+         API_QUERY      TEXT      NOT NULL,
+         GAME           TEXT      NOT NULL,
+         START          TEXT      NOT NULL,
+         EVENTS         TEXT      NOT NULL,
+         MARKETS        INT       NOT NULL,
+         REVENUE        FLOAT     NOT NULL
+         );
+    """
+    create_odds = """
+    CREATE TABLE ODDS
+        (
+         BOOKMAKER  TEXT                  NOT NULL,
+         REGION     TEXT                  NOT NULL,
+         ODDS       TEXT                  NOT NULL,
+         START      TEXT                  NOT NULL,
+         LASTUPDATE TEXT                  NOT NULL,
+         PREV_ENTRY INT,
+         NXT        INT
+        );
+    """
+
+    lr = 0
+    for db_name in ['./active.db', './historical.db']:
+        conn = sqlite3.connect(db_name)
+        cur = conn.cursor()
+        cur.execute(create_bundles)
+        cur.execute(create_odds)
+        lr = cur.lastrowid
+        conn.commit()
+        conn.close()
+        
+        print(f'registered {db_name}')
+
+
+    api.full_update()
+
+
 if __name__ == "__main__":
-    print("START ATTEMPT =========================================================================================================")
     app.run(port=PORT, host="0.0.0.0", debug=False)
     setup()
