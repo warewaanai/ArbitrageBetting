92379520e063d7b77504662e8fed40d6fa628815
Serban Cercelescu <warewaanai@gmail.com>
2021-12-23 02:50:32 +0200

    fixed deployment fup

--- src/main.py
+++ src/main.py
@@ -58,6 +58,7 @@ if __name__ == '__main__':
     app.run(port=PORT, host="0.0.0.0", debug=False)
     tl = Timeloop()
 
+    api.full_update(active, archive)
     @tl.job(interval=timedelta(minutes=20))
     def update():
         api.full_update(active, archive)
